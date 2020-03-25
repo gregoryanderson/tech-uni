8e52a0b92fe9a83c1c43b69fb253f20740289021
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-25 10:36:42 -0600

    Create student migration and add validations

---
+++ app/models/student.rb
@@ -0,0 +1,10 @@
+class Student < ApplicationRecord 
+  before_save {self.email = email.downcase }
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
+
+end 
